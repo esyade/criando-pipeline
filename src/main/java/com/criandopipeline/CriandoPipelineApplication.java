b32eb23b2f89770c80a7260bcc8fb0d8e9e102a1
Erick Syade <e.syadetbo@gmail.com>
2021-05-12 01:21:24 -0300

    Criação do repositório

---
+++ src/main/java/com/criandopipeline/CriandoPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.criandopipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriandoPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriandoPipelineApplication.class, args);
+	}
+
+}
